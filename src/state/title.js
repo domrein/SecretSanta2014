eddd0f113feab9f830e623b858b9aeee2791294d
Paul Milham <domreinalabaster@gmail.com>
2014-12-16 04:41:18 -0700

    added animation on title screen
    Signed-off-by: Paul Milham <domreinalabaster@gmail.com>

--- src/state/title.js
+++ src/state/title.js
@@ -8,13 +8,15 @@ var Title = {
     var roof = this.game.add.tileSprite(0, 380, game.width, game.height - 380, "Building");
     roof.autoScroll(-400, 0);
 
-    var dave = this.game.add.sprite(300, 286);
+    var dave = this.game.add.sprite(290, 286);
     var motoBody = this.game.add.sprite(0, 0, "DaveBikeBack");
     var motoRider = this.game.add.sprite(0, 0, "Dave");
     var motoFront = this.game.add.sprite(0, 0, "DaveBikeFront");
     dave.addChild(motoBody);
     dave.addChild(motoRider);
     dave.addChild(motoFront);
+
+    game.add.tween(dave.position).to({x: 380}, 2500, Phaser.Easing.Sinusoidal.InOut, true, 0, 1000, true);
 
     var titleText = this.game.add.sprite(0, 0, "TitleText");
     titleText.anchor.setTo(0.5, 0.5);
