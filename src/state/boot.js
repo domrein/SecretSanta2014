16bfddceb2dbb07b030dbdce837effff4834a804
Paul Milham <domreinalabaster@gmail.com>
2014-12-18 23:44:30 -0700

    added preloader
    Signed-off-by: Paul Milham <domreinalabaster@gmail.com>

--- src/state/boot.js
+++ src/state/boot.js
@@ -1,5 +1,10 @@
 var Boot = {
   preload: function() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignVertically = true;
+    game.scale.pageAlignHorizontally = true;
+    
+    game.load.image("PreloadBar", "assets/PreloadBar.png");
   },
   create: function() {
     this.game.state.start("Preload");
--- src/state/preload.js
+++ src/state/preload.js
@@ -1,13 +1,13 @@
 var Preload = {
   preload: function() {
+    var preloadBar = game.add.sprite(0, game.height / 2 - 10, "PreloadBar");
+    game.load.setPreloadSprite(preloadBar, 0);
+
     // Load atlas
     game.load.atlas("Sprites", "assets/Sprites.png", "assets/Sprites.json", null, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
     game.load.audio("Music", ["assets/MegaBlaster.mp3", "assets/MegaBlaster.ogg"]);
   },
   create: function() {
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    game.scale.pageAlignVertically = true;
-    game.scale.pageAlignHorizontally = true;
     this.game.state.start("Title");
     var music = game.add.audio("Music", 0.7, true);
     music.play();
